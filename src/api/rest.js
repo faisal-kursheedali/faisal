b5a5b6d9b2190cd2e60ff5e14c2029a500951574
FAISAL <85350108+faisal-kursheedali@users.noreply.github.com>
2024-03-07 18:13:19 +0530

    Update rest.js

--- src/api/rest.js
+++ src/api/rest.js
@@ -4,8 +4,7 @@ import {
   setCollectUserData,
 } from "../app/feature/state";
 
-const SERVER_URL =
-  "https://portfolio-git-dev-faisals-projects-925fdebb.vercel.app";
+const SERVER_URL = "https://faisal-backend.vercel.app/";
 // const SERVER_URL = "http://localhost:3000";
 
 const onLoad = async (date) => {
